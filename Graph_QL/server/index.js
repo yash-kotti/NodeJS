84d1713656472f7aee23d36958102133b7cbb806
yash kotti <yashkotti99@gmail.com>
2024-08-24 17:14:57 +0530

    Created a simple app to show Graph QL
    App with backend in Nodejs and front end in React. In this app I have shown the use of Graph QL by fetching a particular data.

---
+++ Graph_QL/server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+    type User {
+      id:ID!
+      name:String
+      email:String
+      phone:String
+    }
+    type ToDo {
+      id: ID!
+      title: String!
+      completed: Boolean
+      user:User
+    }
+    
+    type Query {
+      getTodos : [ToDo]
+      getAllUsers : [User]
+      getUser(id: ID!) : User
+    }
+    `,
+    resolvers: {
+      ToDo: {
+        user: async (todo) =>
+          (
+            await axios.get(
+              `https://jsonplaceholder.typicode.com/users/${todo.userId}`
+            )
+          ).data,
+      },
+      Query: {
+        getTodos: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+        getAllUsers: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+        getUser: async (parent, { id }) =>
+          (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`))
+            .data,
+      },
+    },
+  });
+
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(8000, () => {
+    console.log(`Server started at PORT 8000`);
+  });
+}
+startServer();
